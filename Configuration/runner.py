dfd82f29a7121559f852a79502dbe212d4b47f24
Kyle Dottin <dottinkyle@gmail.com>
2025-05-05 10:58:39 +0200

    Configuration update

--- Configuration/runner.py
+++ Configuration/runner.py
@@ -2,7 +2,10 @@ import openai
 import traci
 
 sumoBinary= "/home/veins/src/sumo/bin/sumo"
-sumoCmd = [sumoBinary,"-c",""]
+sumoCmd = [sumoBinary,"-c",
+           "Security-agent-in-VANETs-AI-Based-Intrusion-Detection/Configuration/Veins/results/config.sumo.cfg"]
+
+traci.start(sumoCmd)
 
 openai.api_key = "sk-..."  # Remplace par ta clé API
 
