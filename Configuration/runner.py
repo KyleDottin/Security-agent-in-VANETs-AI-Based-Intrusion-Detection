94e43b2844a8b6ddf33bfe994528606bbbd5c084
Kyle Dottin <dottinkyle@gmail.com>
2025-05-05 13:26:28 +0200

    configuration update

--- Configuration/runner.py
+++ Configuration/runner.py
@@ -3,7 +3,7 @@ import traci
 
 sumoBinary= "/home/veins/src/sumo/bin/sumo"
 sumoCmd = [sumoBinary,"-c",
-           "Security-agent-in-VANETs-AI-Based-Intrusion-Detection/Configuration/Veins/results/Basic.sumo.cfg"]
+           "Security-agent-in-VANETs-AI-Based-Intrusion-Detection/Configuration/Veins/src/Basic.sumo.cfg"]
 
 traci.start(sumoCmd)
 
