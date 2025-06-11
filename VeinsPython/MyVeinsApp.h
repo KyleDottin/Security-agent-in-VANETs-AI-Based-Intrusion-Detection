82f9def338fcf0e931e95ff01ed948f505fc8282
Nass951 <nanemiche@gmail.com>
2025-06-11 11:00:44 +0200

    update

---
+++ VeinsPython/MyVeinsApp.h
@@ -0,0 +1,67 @@
+//To place in veins/src/veins/modules/application/traci/MyVeinsApp.h
+// Copyright (C) 2016 David Eckhoff <david.eckhoff@fau.de>
+//
+// Documentation for these modules is at http://veins.car2x.org/
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#pragma once
+
+#include "veins/veins.h"
+
+#include "veins/modules/application/ieee80211p/DemoBaseApplLayer.h"
+#include "veins/modules/messages/BaseFrame1609_4_m.h"
+#include "veins/modules/messages/DemoSafetyMessage_m.h"
+#include <fstream>
+using namespace omnetpp;
+
+namespace veins {
+
+/**
+ * @brief
+ * This is a stub for a typical Veins application layer.
+ * Most common functions are overloaded.
+ * See MyVeinsApp.cc for hints
+ *
+ * @author David Eckhoff
+ *
+ */
+
+class VEINS_API MyVeinsApp : public DemoBaseApplLayer {
+public:
+    void initialize(int stage) override;
+    void finish() override;
+
+protected:
+    void onBSM(DemoSafetyMessage* bsm) override;
+    void onWSM(BaseFrame1609_4* wsm) override;
+    void onWSA(DemoServiceAdvertisment* wsa) override;
+
+    void handleSelfMsg(cMessage* msg) override;
+    void handlePositionUpdate(cObject* obj) override;
+
+    void handleEmergencyMessage(BaseFrame1609_4* wsm);
+    void handleTrafficInfo(BaseFrame1609_4* wsm);
+    void logReceivedMessage(const std::string& senderId, const std::string& data,
+                           simtime_t timestamp, double distance);
+    bool shouldReply(BaseFrame1609_4* wsm);
+    void sendReply(const std::string& originalSenderId);
+
+};
+
+} // namespace veins
