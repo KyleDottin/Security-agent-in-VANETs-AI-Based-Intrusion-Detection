7d7826460ecd69ddb5d3f79478ce64603f1e059f
Kyle Dottin <dottinkyle@gmail.com>
2025-06-18 10:58:00 +0200

    Update

--- VeinsPython/MyVeinsApp.h
+++ VeinsPython/MyVeinsApp.h
@@ -1,25 +1,3 @@
-//To place in veins/src/veins/modules/application/traci/MyVeinsApp.h
-// Copyright (C) 2016 David Eckhoff <david.eckhoff@fau.de>
-//
-// Documentation for these modules is at http://veins.car2x.org/
-//
-// SPDX-License-Identifier: GPL-2.0-or-later
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-
 #pragma once
 
 #include "veins/veins.h"
@@ -27,7 +5,15 @@
 #include "veins/modules/application/ieee80211p/DemoBaseApplLayer.h"
 #include "veins/modules/messages/BaseFrame1609_4_m.h"
 #include "veins/modules/messages/DemoSafetyMessage_m.h"
+
+#include "veins/modules/mobility/traci/TraCIMobility.h"
+#include "veins/modules/mobility/traci/TraCIMobilityAccess.h"
+#include "veins/modules/mobility/traci/TraCICommandInterface.h"
+
 #include <fstream>
+#include <netinet/in.h>  // for sockaddr_in
+#include <string>
+
 using namespace omnetpp;
 
 namespace veins {
@@ -62,6 +48,19 @@ protected:
     bool shouldReply(BaseFrame1609_4* wsm);
     void sendReply(const std::string& originalSenderId);
 
+    // === New Methods and Members for Python UDP Communication ===
+    void initializeUDPSocket();
+    void sendToPython(const std::string& message);
+
+    int udpSocket = -1;
+    struct sockaddr_in pythonAddr;
+
+    Coord curPosition;
+
+    // TraCI interfaces
+    veins::TraCIMobility* mobility = nullptr;
+    veins::TraCICommandInterface* traci = nullptr;
+    veins::TraCICommandInterface::Vehicle* traciVehicle = nullptr;
 };
 
 } // namespace veins
