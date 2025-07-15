fe76558442821cab0f96a82a80581e0ad132691c
Nass951 <nanemiche@gmail.com>
2025-07-15 11:40:04 +0200

    update

---
+++ Nassmcp/agents/blueagent.py
@@ -0,0 +1,18 @@
+
+import asyncio
+from mcp_agent.core.fastagent import FastAgent
+
+fast = FastAgent("BlueAgentHub")  
+@fast.agent(name="Blue Agent",
+            servers=["streamable_http_server"],
+            instruction="""You are an AI agent specialized in SUMO (Simulation of Urban Mobility) traffic simulation and VANET (Vehicular Ad-hoc Network) security analysis.
+Be fast, you just need to call the tools provided by the MCP server to perform your tasks.Be quick and efficient in your responses.
+"""
+)
+async def main():
+
+    async with fast.run() as agent:
+        await agent.interactive()
+
+if __name__ == "__main__":
+    asyncio.run(main())
